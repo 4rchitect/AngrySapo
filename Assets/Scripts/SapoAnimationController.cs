ddedb1b1fdf941ffbaaf278e5f752180099e2607
Will Saymon <wsaymonoficial@gmail.com>
2019-03-30 22:24:45 -0300

    Add Sapo Sprite, Animations and Script

---
+++ Assets/Scripts/SapoAnimationController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Animator))]
+public class SapoAnimationController : MonoBehaviour
+{
+    public event Action OnJumpEvent;
+    public event Action OnFallEvent;
+    public event Action OnGroundEvent;
+
+    private Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Die()
+    {
+
+    }
+
+    public void OnJump()
+    {
+        if (OnJumpEvent != null)
+        {
+            OnJumpEvent.Invoke();
+        }
+        animator.SetTrigger("OnJump");
+    }
+
+    public void OnFall()
+    {
+        if (OnFallEvent != null)
+        {
+            OnFallEvent.Invoke();
+        }
+        animator.SetTrigger("OnFall");
+
+    }
+
+    public void OnGround()
+    {
+        if (OnGroundEvent != null)
+        {
+            OnGroundEvent.Invoke();
+        }
+        animator.SetTrigger("OnGround");
+
+    }
+
+
+}
