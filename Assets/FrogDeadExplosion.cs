8c7256bed46b100fb4b7310efb92817aa14d99d6
Will Saymon <wsaymonoficial@gmail.com>
2019-03-31 16:10:54 -0300

    asdasdjkask vai sapo morre

---
+++ Assets/FrogDeadExplosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FrogDeadExplosion : MonoBehaviour {
+
+	public float force;
+	// public Layer deadFrogLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
